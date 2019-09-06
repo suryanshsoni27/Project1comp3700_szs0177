4a8e012e4a70c0f8c2e08e30aa75b5ba24279789
suryansh soni <49648748+suryanshsoni27@users.noreply.github.com>
2019-09-06 03:10:53 -0500

    here

--- openkattis/src/main/java/sodaslurper.java
+++ openkattis/src/main/java/sodaslurper.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class sodaslurper {
+class soda_sodasurpler {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //System.out.print("Input");
+        int e= scan.nextInt() + scan.nextInt();
+        //System.out.print("Input");
+        int b = 0;
+        //System.out.print("Input");
+        int req = scan.nextInt();
+        int total = 0;
+        while(e>=req) {
+            b = e%req;
+            e = e/req;
+            total = total + e;
+            e = e + b;
+
+
+        }
+        System.out.println(total);
+
+
+    }
+
 }
